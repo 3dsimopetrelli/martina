85df8f47966024b2a7d28374fb39748365f9cd39
Simo <3dsimopetrelli@gmail.com>
2026-05-08 12:36:39 +0200

    Update template-link-page.php

--- includes/modules/link-page/templates/template-link-page.php
+++ includes/modules/link-page/templates/template-link-page.php
@@ -336,7 +336,17 @@ if ($background_gradient_enabled) {
                     <?php echo '' !== $render_link['rel'] ? ' rel="' . esc_attr($render_link['rel']) . '"' : ''; ?>>
                     <?php if ('email' === $render_link['type']) : ?>
                         <span class="link-item-email-title">
-                            <?php if (!empty($render_link['show_mail_icon'])) : ?><span class="link-item-email-icon" aria-hidden="true">&#9993;</span><?php endif; ?>
+                            <?php if (!empty($render_link['show_mail_icon'])) : ?>
+                                <span class="link-item-email-icon" aria-hidden="true">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M14 4.1 12 6"/>
+                                        <path d="m5.1 8-2.9-.8"/>
+                                        <path d="m6 12-1.9 2"/>
+                                        <path d="M7.2 2.2 8 5.1"/>
+                                        <path d="M9.037 9.69a.498.498 0 0 1 .653-.653l11 4.5a.5.5 0 0 1-.074.949l-4.349 1.041a1 1 0 0 0-.74.739l-1.04 4.35a.5.5 0 0 1-.95.074z"/>
+                                    </svg>
+                                </span>
+                            <?php endif; ?>
                             <span><?php echo esc_html($render_link['label']); ?></span>
                         </span>
                         <span class="link-item-email-address"><?php echo esc_html($render_link['email']); ?></span>
